56eb05bb1b54d0eeccef13ddc42d9d54c9a3a29d
Shaun <shaun@thenetninja.co.uk>
2022-08-31 16:44:46 +0100

    lesson-4

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,28 @@
       <h1>Pinia Tasks</h1>
     </header>
 
+    <!-- task list -->
+    <div class="task-list">
+      <div v-for="task in taskStore.tasks" :key="task.id">
+        <!-- <p>{{ task.title }}</p> -->
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+
   </main>
 </template>
 
 <script>
+  import TaskDetails from './components/TaskDetails.vue'
+
+  import { useTaskStore } from './stores/TaskStore'
+
   export default {
-    
+    components: { TaskDetails },
+    setup() {
+      const taskStore = useTaskStore()
+
+      return { taskStore }
+    }
   }
 </script>
---
+++ src/components/TaskDetails.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="task">
+    <h3>{{ task.title }}</h3>
+    <div class="icons">
+      <i class="material-icons">delete</i>
+      <i class="material-icons">favorite</i>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['task'],
+  }
+</script>
