14eaeb86985576ca60f7fd53397c45d2834c499e
Shaun <shaun@thenetninja.co.uk>
2022-08-31 10:31:26 +0100

    lesson-2

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+
+    <!-- heading -->
+    <header>
+      <img src="https://pinia.vuejs.org/logo.svg" alt="pinia logo">
+      <h1>Pinia Tasks</h1>
+    </header>
+
+  </main>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
