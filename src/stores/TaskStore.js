6d24c54ab57ddf0da175f6103c9bb74e8217bf9f
Shaun <shaun@thenetninja.co.uk>
2022-09-02 10:30:51 +0100

    lesson-9

--- src/stores/TaskStore.js
+++ src/stores/TaskStore.js
@@ -29,17 +29,45 @@ export const useTaskStore = defineStore('taskStore', {
       this.tasks = data
       this.loading = false
     },
-    addTask(task) {
+    async addTask(task) {
       this.tasks.push(task)
+
+      const res = await fetch('http://localhost:3000/tasks', {
+        method: 'POST',
+        body: JSON.stringify(task),
+        headers: {'Content-Type': 'application/json'}
+      })
+
+      if (res.error) {
+        console.log(res.error)
+      }
     },
-    deleteTask(id) {
+    async deleteTask(id) {
       this.tasks = this.tasks.filter(t => {
         return t.id !== id
       })
+
+      const res = await fetch('http://localhost:3000/tasks/' + id, {
+        method: 'DELETE',
+      })
+
+      if (res.error) {
+        console.log(res.error)
+      }
     },
-    toggleFav(id) {
+    async toggleFav(id) {
       const task = this.tasks.find(t => t.id === id)
       task.isFav = !task.isFav
+
+      const res = await fetch('http://localhost:3000/tasks/' + id, {
+        method: 'PATCH',
+        body: JSON.stringify({ isFav: task.isFav }),
+        headers: {'Content-Type': 'application/json'}
+      })
+
+      if (res.error) {
+        console.log(res.error)
+      }
     }
   }
 })
